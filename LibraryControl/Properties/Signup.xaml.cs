30e73e863ac6ddbc2219405c2686cc50b0eae6b8
Alaa Ahmed <Alaa Ahmed@DESKTOP-M61D6LM>
2026-07-27 21:04:46 +0300

    Add project files.

---
+++ LibraryControl/Properties/Signup.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryControl.Properties
+{
+    /// <summary>
+    /// Interaction logic for Signup.xaml
+    /// </summary>
+    public partial class Signup : Window
+    {
+        MyLibraryEntities context = new MyLibraryEntities();
+        public Signup()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
